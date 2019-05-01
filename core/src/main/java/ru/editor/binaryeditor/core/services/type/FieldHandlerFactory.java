b85d913824662c0b6727cc0e8af2c4f887a95370
redbore <mariyzdede@gmail.com>
2019-05-02 02:10:38 +0300

    added new types

--- core/src/main/java/ru/editor/binaryeditor/core/services/type/FieldHandlerFactory.java
+++ core/src/main/java/ru/editor/binaryeditor/core/services/type/FieldHandlerFactory.java
@@ -7,13 +7,18 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static ru.editor.binaryeditor.core.services.type.TypeConverter.*;
+
 public class FieldHandlerFactory {
 
     private final Map<String, FieldHandler> handlers = new HashMap<>();
 
     private void init() {
+        handlers.put("int16", new FieldHandler(this::readInt16, this::writeInt16));
         handlers.put("int32", new FieldHandler(this::readInt32, this::writeInt32));
+        handlers.put("int64", new FieldHandler(this::readInt64, this::writeInt64));
         handlers.put("float32", new FieldHandler(this::readFloat32, this::writeFloat32));
+        handlers.put("float64", new FieldHandler(this::readFloat64, this::writeFloat64));
         handlers.put("string", new FieldHandler(this::readString, this::writeString));
     }
 
@@ -25,14 +30,32 @@ public class FieldHandlerFactory {
         return handlers.get(type).writer();
     }
 
+    private Short readInt16(byte[] bytes, AtomicInteger offset, Integer length) {
+        short value = ByteBuffer.wrap(bytes, offset.get(), Short.BYTES).order(ByteOrder.LITTLE_ENDIAN).getShort();
+        offset.addAndGet(length);
+        return value;
+    }
+
     private Integer readInt32(byte[] bytes, AtomicInteger offset, Integer length) {
         int value = ByteBuffer.wrap(bytes, offset.get(), Integer.BYTES).order(ByteOrder.LITTLE_ENDIAN).getInt();
         offset.addAndGet(length);
         return value;
     }
 
+    private Long readInt64(byte[] bytes, AtomicInteger offset, Integer length) {
+        long value = ByteBuffer.wrap(bytes, offset.get(), Long.BYTES).order(ByteOrder.LITTLE_ENDIAN).getLong();
+        offset.addAndGet(length);
+        return value;
+    }
+
     private Float readFloat32(byte[] bytes, AtomicInteger offset, Integer length) {
-        float value = ByteBuffer.wrap(bytes, offset.get(), Integer.BYTES).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+        float value = ByteBuffer.wrap(bytes, offset.get(), Float.BYTES).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+        offset.addAndGet(length);
+        return value;
+    }
+
+    private Double readFloat64(byte[] bytes, AtomicInteger offset, Integer length) {
+        double value = ByteBuffer.wrap(bytes, offset.get(), Double.BYTES).order(ByteOrder.LITTLE_ENDIAN).getDouble();
         offset.addAndGet(length);
         return value;
     }
@@ -43,50 +66,28 @@ public class FieldHandlerFactory {
         return value;
     }
 
+    private void writeInt16(ByteBuffer buffer, Object value, Integer length) {
+        buffer.order(ByteOrder.LITTLE_ENDIAN).putShort(toShort(value));
+    }
+
     private void writeInt32(ByteBuffer buffer, Object value, Integer length) {
         buffer.order(ByteOrder.LITTLE_ENDIAN).putInt(toInt(value));
+    }
+
+    private void writeInt64(ByteBuffer buffer, Object value, Integer length) {
+        buffer.order(ByteOrder.LITTLE_ENDIAN).putLong(toLong(value));
     }
 
     private void writeFloat32(ByteBuffer buffer, Object value, Integer length) {
         buffer.order(ByteOrder.LITTLE_ENDIAN).putFloat(toFloat(value));
     }
 
+    private void writeFloat64(ByteBuffer buffer, Object value, Integer length) {
+        buffer.order(ByteOrder.LITTLE_ENDIAN).putDouble(toDouble(value));
+    }
+
     private void writeString(ByteBuffer buffer, Object value, Integer length) {
-        buffer.order(ByteOrder.BIG_ENDIAN).put(fixSizeString(toString(value), length).getBytes());
+        buffer.order(ByteOrder.BIG_ENDIAN).put(toFixSizeString(TypeConverter.toString(value), length).getBytes());
     }
 
-    private static Integer toInt(Object value) {
-        if (value instanceof String) {
-            return Integer.parseInt((String) value);
-        }
-        if (value instanceof Integer) {
-            return (Integer) value;
-        }
-        return 0;
-    }
-
-    private static Float toFloat(Object value) {
-        if (value instanceof String) {
-            return Float.parseFloat((String) value);
-        }
-        if (value instanceof Float) {
-            return (Float) value;
-        }
-        return 0F;
-    }
-
-    private static String toString(Object value) {
-        return value instanceof String ? (String) value : value.toString();
-    }
-
-    private static String fixSizeString(String value, Integer length) {
-        StringBuilder stringBuilder = new StringBuilder(value);
-        if (value.length() < length) {
-            int needSize = length - value.length();
-            for (int i = 0; i < needSize; i++) {
-                stringBuilder.append("\u0000");
-            }
-        }
-        return stringBuilder.toString();
-    }
 }
---
+++ core/src/main/java/ru/editor/binaryeditor/core/services/type/TypeConverter.java
@@ -0,0 +1,68 @@
+package ru.editor.binaryeditor.core.services.type;
+
+public final class TypeConverter {
+    public static Short toShort(Object value) {
+        if (value instanceof String) {
+            return Short.parseShort((String) value);
+        }
+        if (value instanceof Short) {
+            return (Short) value;
+        }
+        return 0;
+    }
+
+    public static Integer toInt(Object value) {
+        if (value instanceof String) {
+            return Integer.parseInt((String) value);
+        }
+        if (value instanceof Integer) {
+            return (Integer) value;
+        }
+        return 0;
+    }
+
+    public static Long toLong(Object value) {
+        if (value instanceof String) {
+            return Long.parseLong((String) value);
+        }
+        if (value instanceof Long) {
+            return (Long) value;
+        }
+        return 0L;
+    }
+
+    public static Float toFloat(Object value) {
+        if (value instanceof String) {
+            return Float.parseFloat((String) value);
+        }
+        if (value instanceof Float) {
+            return (Float) value;
+        }
+        return 0F;
+    }
+
+    public static Double toDouble(Object value) {
+        if (value instanceof String) {
+            return Double.parseDouble((String) value);
+        }
+        if (value instanceof Double) {
+            return (Double) value;
+        }
+        return 0.0;
+    }
+
+    public static String toString(Object value) {
+        return value instanceof String ? (String) value : value.toString();
+    }
+
+    public static String toFixSizeString(String value, Integer length) {
+        StringBuilder stringBuilder = new StringBuilder(value);
+        if (value.length() < length) {
+            int needSize = length - value.length();
+            for (int i = 0; i < needSize; i++) {
+                stringBuilder.append("\u0000");
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
